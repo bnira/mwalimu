1b6cf7456fbb98b6687ef54db90d77eb2161a3a1
mamba <isaiahntina47@gmail.com>
2024-06-06 03:12:28 +0000

    refactor: Add API endpoint to check if an employee exists

---
+++ pages/api/checkEmployee.js
@@ -0,0 +1,24 @@
+//path: /api/checkEmployee
+import { superbase } from "@/lib/supabaseClient";
+
+/**
+ * Handler function to check if an employee exists in the voters table.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @returns {Promise<void>} - A promise that resolves when the response is sent.
+ */
+export default async function handler(req,res) {
+    const {employeeId} = req.body;
+
+    const {data, error} = await superbase
+    .from('voters')
+    .select('*')
+    .eq('employee_id', employeeId)
+    .single()
+
+    if (error || !data) {
+        res.status(404).json({messsage: 'Employee not found'});
+    } else {
+        res.status(200).json({message: 'Employee found'});
+    }
+}

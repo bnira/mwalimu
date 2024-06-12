20853a60ba2e66bbbce41e6b66df2379a8565931
Boniface <bthura@outlook.com>
2024-06-11 19:24:58 -0700

    adim page changes

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -5,36 +5,36 @@ import { AdminLoginComponent } from '../components/adminLoginComponent';
 import '../app/globals.css';
 
 const Invitation: React.FC = () => {
- const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
- useEffect(() => {
-  const isLoggedIn = sessionStorage.getItem('isLoggedIn') === 'true';
-  setIsAuthenticated(isLoggedIn);
- }, []);
+  useEffect(() => {
+    const isLoggedIn = sessionStorage.getItem('isLoggedIn') === 'true';
+    setIsAuthenticated(isLoggedIn);
+  }, []);
 
- const handleLoginSuccess = () => {
-  setIsAuthenticated(true);
- };
+  const handleLoginSuccess = () => {
+    setIsAuthenticated(true);
+  };
 
- return isAuthenticated ? (
+  return isAuthenticated ? (
     <div>
-        <nav className="bg-gray-800 p-4">
+      <nav className="bg-gray-800 p-4">
         <div className="container mx-auto flex justify-between items-center">
           <div className="text-white font-bold">Admin DashBoard</div>
           <div>
             <Link href="/admin">
-              <div className="text-white">Admin</div>
+              <div className="text-white">Go to Home</div>
             </Link>
           </div>
         </div>
       </nav>
       <div className='flex flex-col items-center justify-center min-h-screen py-2'>
         <AdminDash />
+      </div>
     </div>
-    </div>
- ) : (
-  <AdminLoginComponent onLoginSuccess={handleLoginSuccess} />
- );
+  ) : (
+    <AdminLoginComponent onLoginSuccess={handleLoginSuccess} />
+  );
 };
 
 export default Invitation;
